ab2410b7035857083cdc6c953642a0ab3e9b946c
Trzyszcz <marcel.windys@gmail.com>
2017-02-27 23:17:36 +0100

    Changing some unimportant stuff

--- main_to_satori.py
+++ main_to_satori.py
@@ -2,10 +2,8 @@ import requests
 
 address = input()
 tests = requests.get(address)
-try:
-    tests.raise_for_status()
-except Exception as exc:
-    print(exc)
+
+tests.raise_for_status()
 
 playFile = open('tests.zip', 'wb')
 for chunk in tests.iter_content(100000):
