a7aee216e214b2cb7bfce34e680cf3ace48d40d3
Trzyszcz <marcel.windys@gmail.com>
2017-03-29 23:45:11 +0200

    Now program extracts downloaded zipfile into a new directory

--- main_to_satori.py
+++ main_to_satori.py
@@ -1,4 +1,6 @@
 import requests
+import zipfile
+import os
 
 address = input()
 tests = requests.get(address)
@@ -9,3 +11,13 @@ playFile = open('tests.zip', 'wb')
 for chunk in tests.iter_content(100000):
     playFile.write(chunk)
 playFile.close()
+
+path0 = os.getcwd() + '/Tests_main'
+
+os.makedirs(path0)
+
+Tests_file = zipfile.ZipFile('tests.zip', 'r')
+Tests_file.extractall(path0)
+Tests_file.close()
+
+
